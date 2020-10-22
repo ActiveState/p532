7368f8fa584189d7cc1327cbfcdf6e9f0962c18c
lukep <luke.pacholski@gmail.com>
2020-10-22 08:39:01 -0500

    Added platform detection to the CLI inputs

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+// Set up platform toggle for the CLI command
+
+let platformToggles = document.querySelectorAll(".platform-picker span");
+let cliInputs = document.querySelectorAll(".cli-install input");
+let defaultPlatform = 'Windows';
+let selectedPlatform = defaultPlatform;
+
+let cliCommands = {
+  'Linux' : 'sh <(curl -q https://platform.activestate.com/dl/cli/install.sh)',
+  'Windows' : 'powershell -- sh <(curl -q https://platform.activestate.com/dl/cli/install.sh)'
+}
+
+
+platformToggles.forEach(el => {
+  el.addEventListener("click", e => {
+    switchPlatform(el.getAttribute("platform"));
+  })
+});
+
+const switchPlatform = platform => {
+
+  platform = cliCommands[platform] ? platform : defaultPlatform;
+
+  platformToggles.forEach(el => {
+    let thisPlatform = el.getAttribute("platform");
+    
+    if(thisPlatform == platform) {
+      el.classList.add("selected");
+    } else {
+      el.classList.remove("selected");
+    }
+  });
+
+  cliInputs.forEach(el => {
+    el.value = cliCommands[platform];
+  })
+}
+
+switchPlatform(getOS());
+
+
+
+
+
+
 let root = document.documentElement;
 
 window.addEventListener("scroll", e => {
@@ -160,4 +205,27 @@ const updateItems = (entries, observer, elements, className) => {
       // observer.disconnect();
     }
   }
+}
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
 }
