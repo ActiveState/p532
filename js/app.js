9d6fe1ff27b883d534414dd4eb22251d766f3728
lukep <luke.pacholski@gmail.com>
2020-11-06 10:16:23 -0600

    Package animation update

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ Platforms.setDefaultPlatform();
 // Sets up all the intersectioon observers
 import * as Observer from './observer.js';
 Observer.simple(".bundles-section");
-Observer.simple(".history-section");
 Observer.simple(".environments-section");
+Observer.simple(".history-section");
 Observer.simple(".virtual-env-section");
-Observer.simple(".build-progress-section");
-Observer.simple(".license-section");
-Observer.simple(".cli-section");
 Observer.simple(".dependencies-section");
+Observer.simple(".build-progress-section");
+Observer.simple(".cli-section");
+Observer.simple(".license-section");
